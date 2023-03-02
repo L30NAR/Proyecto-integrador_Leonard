cc04e790a82a8bdc213c1b33965cf451523b6daa
Leonard <108544224+L30NAR@users.noreply.github.com>
2023-03-02 06:37:56 -0300

    actuali

--- Front-end/Proyecto_int_Leonard/src/app/app.module.ts
+++ Front-end/Proyecto_int_Leonard/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    BodyComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
